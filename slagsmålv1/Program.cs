9d6abb1b2a42c1468074af39b5a702e1a56fd987
Charlie Sesser Nylund <charlie.sessernylun@learnet.se>
2020-10-12 08:54:10 +0200

    first commit

---
+++ slagsmålv1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace slagsmålv1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fighterhealth1 = 20;
+            int fighterhealth2 = 20;
+
+            Console.WriteLine("Tjena kom hit och slåss!\n-------------------------------");
+            Console.WriteLine("Mike Tyson vs. King DEDE");
+
+            while (fighterhealth1 != 0 || fighterhealth2 != 0)
+            {
+
+                Random Generator = new Random();
+
+
+
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
